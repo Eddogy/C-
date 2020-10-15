cc5941503edebca2ed57d64a252688a61721f151
Edgaras Mitkus <Erainius@yahoo.co.uk>
2020-10-15 11:32:55 +0300

    Skaitykla

---
+++ Skaitykla/Skaitykla.MVC/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Skaitykla.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Skaitykla.MVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.AuthError = "blogi prisijungimo duomenys";
+                return View("~/Views/Auth/Index.cshtml");
+            }
+
+            if (model.Email == "test@admin" && model.Password == "Pass")
+            {
+                return RedirectToAction("Admin");
+            }
+
+            return View();
+        }
+
+        public IActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
